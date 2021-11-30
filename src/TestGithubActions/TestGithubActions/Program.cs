81925a2fc4f81090dcfb93e969496599ae9f3701
Bart Kardol <b.kardol@hotmail.com>
2021-11-30 11:23:30 +0100

    initial test

---
+++ src/TestGithubActions/TestGithubActions/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace TestGithubActions
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
